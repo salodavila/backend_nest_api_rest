6c472096cfe12b6ae64c871191a8d3c70c70b699
Salo <msds.salo.davila@gmail.com>
2026-02-17 20:01:17 -0500

    Configuración inicial: Base de datos TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST || 'localhost',
+      port: +`${process.env.DATABASE_PORT}` || 5432,
+      username: process.env.DATABASE_USER || 'postgres',
+      password: process.env.DATABASE_PASSWORD || 'postgres',
+      database: process.env.DATABASE_NAME || 'bd_back_nest_api',
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      synchronize: false
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
